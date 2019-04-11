7237ff12341a7153b7cf6305d5f778db0944b301
German Montejo <gemontejo@gmail.com>
2019-04-11 23:43:34 +0800

    Add pomodoro.py for datetime experiments.

---
+++ pomodoro.py
@@ -0,0 +1,44 @@
+import time
+from datetime import datetime, timedelta
+
+def sleep():
+    pomodoro_tracker = 0
+
+    # flip is going to be used to switch the end time to 25 mins or 5 mins
+    # when the ctr resets to 0.
+    flip = 0
+    print('Begin...')
+
+    end = datetime.now() + timedelta(minutes=25)
+    while True:
+        ctr = (end-datetime.now()) + timedelta(seconds=1)
+
+        print(str(ctr).split('.')[0])
+
+        if ctr.seconds == 0:
+            pomodoro_tracker += 1
+            flip += 1
+
+            if pomodoro_tracker > 4:
+                # after 4 pomodoro, take longer breaks, i.e. 20 mins
+                end = datetime.now() + timedelta(minutes=20)
+                pomodoro_tracker = 0
+                print('long break countdown...')
+
+            else:
+                # this means we're back to 25 mins pomodoro countdown
+                if flip % 2 == 0:
+                    end = datetime.now() + timedelta(minutes=25)
+                    print('pomodoro countdown...')
+                else:
+                    # else, countdown is on short break
+                    end = datetime.now() + timedelta(minutes=5)
+                    print('short break countdown...')
+
+        time.sleep(1)
+
+try:
+    sleep()
+except KeyboardInterrupt:
+    print('Exiting program...')
+    exit()
